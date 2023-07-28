c4fc31c10cca39dddd6cee1e407b31aa9b34e86c
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-07-27 18:36:34 -0700

    added more code

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 
 // 3rd Party Resources
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 // Esoteric Resources
-const errorHandler = require("./error-handlers/500.js");
-const notFoundHandler = require("./error-handlers/404.js");
-const authRoutes = require("./auth/routes.js");
-const v1Routes = require("./routes/v1.js");
-const logger = require("./middleware/logger.js");
+const errorHandler = require('./error-handlers/500.js');
+const notFoundHandler = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes.js');
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
+const logger = require('./middleware/logger.js');
 // Prepare the express app
 const app = express();
 
@@ -22,18 +23,19 @@ app.use(logger);
 
 // Routes
 app.use(authRoutes);
-app.use("/api/v1", v1Routes);
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 //localhost:3000/api/v1/food/2
 
 // Catchalls
-app.use("*", notFoundHandler);
+app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 module.exports = {
   server: app,
   start: (port) => {
     if (!port) {
-      throw new Error("Missing Port");
+      throw new Error('Missing Port');
     }
     app.listen(port, () => console.log(`Listening on ${port}`));
   },
